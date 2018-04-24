109b7e4ae50c5fb36adcb0efa16e3810945e7fcf
Julius Saputra <julius_saputra@hotmail.com>
2018-04-24 13:20:05 +0200

    added icon and splash screen

--- app/src/main/java/com/eduid/EduIdApp/view/MainActivity.java
+++ app/src/main/java/com/eduid/EduIdApp/view/MainActivity.java
@@ -3,6 +3,7 @@ package com.eduid.EduIdApp.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.eduid.EduIdApp.R;
 import com.eduid.EduIdApp.controller.ActivitiesManager;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,8 +11,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        ActivitiesManager.startLoginActivity(this);
+         setContentView(R.layout.activity_main);
+
+        //ActivitiesManager.startLoginActivity(this);
     }
 
 }
